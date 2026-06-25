8eec0b49231dcb9890598596b9d13c5d66efef46
Lisbeth Floreano <tucorreo@gmail.com>
2026-06-25 16:00:58 -0500

    Cambios en mi API y archivos eliminados

--- src/config.js
+++ src/config.js
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const DB_HOST = process.env.DB_HOST;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_PORT = process.env.DB_PORT;
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD ||'';
+export const DB_DATABASE = process.env.DB_DATABASE;'base2026';
+export const DB_PORT = process.env.DB_PORT || 3306;
+
+export const PORT = process.env.PORT || 3000;
